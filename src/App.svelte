a6a5c0ce96ab7c25a4ce3b0a7f47b4d1daf8dc2a
Elisabeth ROBERT AOUCHER <elisabethrobertaoucher@gmail.com>
2024-01-16 17:31:19 +0100

    ajouts

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Counter from "./lib/Counter.svelte"
+</script>
+
+<Counter />
+
+
+<style>
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,27 @@
+<script>
+  let count = 0
+  let string;
+  let string2;
+
+   $ : {
+     string = `You have clicked ${count} times`
+   }
+
+   $ : string2 = `You have ${count} times`
+
+  function increment(){
+    count+=1
+  }
+</script>
+
+<button on:click={increment}>Clicks {count}</button>
+<h3>{string}</h3>
+<h3>{string2}</h3>
+<h3>You have clicked {count} times</h3>
+
+<style>
+  button{
+    padding:15px 20px;
+    cursor:pointer
+  }
+</style>
